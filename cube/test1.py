fd60066861a0d8fc768e59e731a2bd69db7254b7
Raven3006 <103230345+Raven3006@users.noreply.github.com>
2023-04-20 20:39:42 +0800

    Delete test1.py

--- cube/test1.py
+++
@@ -1,8 +0,0 @@
-from cube import *
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-c0=cube()
-a=torch.tensor(c0.arr.flatten().astype(np.int64))
-print(a-1)
-print(F.one_hot(a-1))
